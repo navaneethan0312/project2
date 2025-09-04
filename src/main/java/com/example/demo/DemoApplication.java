2480baaf9d1460b754185185d4d0337e70e6cfee
SpringDev-ai <palvikram73@gmail.com>
2025-09-04 18:36:46 +0530

    2 trigger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author Vikram Pal
  * Main application class for Spring Boot application.
+ * This class bootstraps the application.
  */
 @SpringBootApplication
 public class DemoApplication {
